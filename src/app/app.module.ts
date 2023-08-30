6422a8202715bba2098e32c06798f18faecc6524
SuperArmadilloDev <follietmartin@gmail.com>
2023-08-30 10:01:38 +0200

    style and app component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,42 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+
+import { ReactiveFormsModule } from '@angular/forms';
+
+import { StoreModule } from '@ngrx/store';
+
+import { GridComponent } from '@app/components/grid/grid.component';
+import { PageSelectComponent } from '@app/components/page-select/page-select.component';
+
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import {MatInputModule} from '@angular/material/input';
+import { EffectsModule } from '@ngrx/effects';
+
+import { HttpClientModule } from '@angular/common/http';
+import { reducers, effects } from './store/beer-data';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
+    HttpClientModule,
+    MatIconModule,
+    MatButtonModule,
+    MatInputModule,
+    PageSelectComponent,
+    GridComponent,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    ReactiveFormsModule,
+    StoreModule.forRoot(reducers),
+    EffectsModule.forRoot(effects),
+    StoreDevtoolsModule.instrument()
   ],
   providers: [],
   bootstrap: [AppComponent]
